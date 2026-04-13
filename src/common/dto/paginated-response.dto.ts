bef325bc486ece0ec2d4cfbee4e48e0c1c15f7ae
Alexei Apostol <alexapostol.m@gmail.com>
2026-04-13 22:58:53 +0300

    fixes

--- src/common/dto/paginated-response.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class PaginatedResponseDto<TData> {
-  @ApiProperty()
-  count: number;
-
-  items: TData[];
-}
--- src/common/utils/find-with-count-mapping.ts
+++
@@ -1,9 +0,0 @@
-import { PaginatedResponseDto } from '../dto/paginated-response.dto';
-
-export function findWithCountMapping<T, TExtra = object>(
-  items: T[],
-  count: number,
-  extra?: TExtra,
-): PaginatedResponseDto<T> & TExtra {
-  return { items, count, ...(extra ?? {}) } as PaginatedResponseDto<T> & TExtra;
-}
